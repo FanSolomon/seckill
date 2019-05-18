5474ec98cdfb560b882d3b5e8c698f986f3005a1
FanSolomon <jetonly@hotmail.com>
2019-05-18 23:10:27 +0800

    prepare4JMeter

--- src/main/java/com/ytz/seckill/controller/LoginController.java
+++ src/main/java/com/ytz/seckill/controller/LoginController.java
@@ -33,11 +33,10 @@ public class LoginController {
 
     @RequestMapping("/do_login")
     @ResponseBody
-    public Result<Boolean> doLogin(HttpServletResponse response, @Valid LoginVo loginVo) {
+    public Result<String> doLogin(HttpServletResponse response, @Valid LoginVo loginVo) {
         log.info(loginVo.toString());
-//        System.out.println(loginVo.toString());
         //登录
-        userService.login(response, loginVo);
-        return Result.success(true);
+        String token = userService.login(response, loginVo);
+        return Result.success(token);
     }
 }
---
+++ src/main/java/com/ytz/seckill/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.ytz.seckill.controller;
+
+import com.ytz.seckill.domain.MiaoshaUser;
+import com.ytz.seckill.redis.RedisService;
+import com.ytz.seckill.result.Result;
+import com.ytz.seckill.service.MiaoshaUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    MiaoshaUserService userService;
+
+    @Autowired
+    RedisService redisService;
+
+    @RequestMapping("/info")
+    @ResponseBody
+    public Result<MiaoshaUser> info(Model model, MiaoshaUser user) {
+        return Result.success(user);
+    }
+
+}
